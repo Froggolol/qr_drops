c76d892a33f20c70c82a2a354e3a17f47a4296a9
Froggo <peter1337n@gmail.com>
2022-01-28 22:45:10 +0100

    So we begin

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
+import uvicorn
 from fastapi import FastAPI
-
+import sqlalchemy as db
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
+engine = db.create_engine('sqlite:///qr_code_urls.db')
+connection = engine.connect()
+metadata = db.MetaData()
+
+@app.get("/sizes")
+async def return_sizes():
+    sizes = db.Table('sizes', metadata, autoload=True, autoload_with=engine)
+    t = connection.execute(db.select([sizes]).where(sizes.columns.quantity > 0))
+    for row in t.fetchall():
+        print(row._asdict())
     return {"message": "Hello World"}
 
-def scan():
-    return
 
-
-def check_in_file():
-    return
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=5049)
