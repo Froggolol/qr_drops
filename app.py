7ce9b23cea35e74d17943411195c1bf7fb1e9b93
Froggo <peter1337n@gmail.com>
2022-01-29 13:39:21 +0100

    So we continue

--- app.py
+++ app.py
@@ -1,21 +1,44 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 import sqlalchemy as db
-
+from pydantic import BaseModel
+from datetime import datetime
 app = FastAPI()
 
 engine = db.create_engine('sqlite:///qr_code_urls.db')
 connection = engine.connect()
 metadata = db.MetaData()
+sizes = db.Table('sizes', metadata, autoload=True, autoload_with=engine)
+
+
+class Reservation(BaseModel):
+    name: str
+    phone: str
+    url: str
+    isClaimed: int
+    claimedDatetime: datetime
+    size: str
+    updated: datetime
+    callCount: int
+
 
 @app.get("/sizes")
-async def return_sizes():
-    sizes = db.Table('sizes', metadata, autoload=True, autoload_with=engine)
-    t = connection.execute(db.select([sizes]).where(sizes.columns.quantity > 0))
-    for row in t.fetchall():
-        print(row._asdict())
-    return {"message": "Hello World"}
+async def return_sizes(request: Request):
+    if is_not_claimed():
+        t = connection.execute(db.select([sizes]).where(sizes.columns.quantity > 0))
+        sizes_available = t.fetchall()
+        return sizes_available
+    else:
+        return {'error': True, 'message': 'This code has already been used try to find another code GOOD LUCK!'}
+
+
+@app.post("/sizes/{size}")
+async def confirm_size(size, reservation: Reservation):
+    t = connection.execute(db.select([sizes]).where(sizes.columns.size == size))
+    sizes_available = t.fetchall()
+    print(reservation.name)
+    return sizes_available
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=5049)
+    uvicorn.run(app, host="127.0.0.1", port=8080)
