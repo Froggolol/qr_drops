005b6bf7a554a5322a510b7d8ba3c699c7ae6a9b
Froggo <peter1337n@gmail.com>
2022-01-29 17:59:48 +0100

    Changed the way we register sizes, and removed the count from the response.

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import uvicorn
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 import sqlalchemy as db
 from pydantic import BaseModel
 from datetime import datetime
+from collections import ChainMap
+
 app = FastAPI()
 
 engine = db.create_engine('sqlite:///qr_code_urls.db')
@@ -21,21 +23,25 @@ class Reservation(BaseModel):
 
 
 @app.get("/sizes")
-async def return_sizes(request: Request, url_token):
-    if _is_not_claimed(url_token):
-        t = connection.execute(db.select([sizes]).filter(sizes.columns.quantity > 0))
-        sizes_available = t.fetchall()
-        return sizes_available
+async def return_sizes(url_token):
+    if _is_not_claimed(url_token, count=False):
+        return _sizes_left()
     else:
         return {'error': True, 'message': 'This code has already been used try to find another code GOOD LUCK!'}
 
 
 @app.post("/reserve/")
 async def confirm_size(reservation: Reservation):
-    if _is_not_claimed(reservation.url_token, count=False) == False:
+    try:
+        assert len(reservation.phone) == 8
+        assert 2 <= len(reservation.size.replace('.','')) <= 3
+    except AssertionError:
+        return {'error': True, 'message': 'Phonenumber must be 8 characters long example: 12345678 and size must be from the list'}
+
+    if not _is_not_claimed(reservation.url_token):
         return {'error': True, 'message': 'This code has already been used try to find another code GOOD LUCK!'}
 
-    if size_reservation(reservation.size):
+    if _size_reservation(reservation.size):
         stmt = claimed.update().where(claimed.columns.url_token == reservation.url_token).values(
             name=reservation.name,
             phone=reservation.phone,
@@ -49,9 +55,7 @@ async def confirm_size(reservation: Reservation):
         if response.rowcount == 1:
             return {'error':False, 'message': f'CONGRATS {reservation.name} you got a size: {reservation.size} waiting for you to pickup at Enghaven'}
         else:
-            stmt = sizes.update().where(sizes.columns.size == reservation.size).values(quantity=(sizes.columns.quantity + 1))
-            connection.execute(stmt)
-            return {'error': True, 'message': 'Something went wrong not sure what try again, maybe the url was already used'}
+            return {'error': True, 'message': 'Something went wrong not sure what try again, maybe the url was already used, try again?'}
     else:
         return {'error': True,
                 'message': 'Unlucky someone else has claimed that size, try another one. '}
@@ -68,19 +72,33 @@ def _is_not_claimed(url_token, count=True):
     return False
 
 
-def size_reservation(size):
-    t = connection.execute(db.select([sizes]).filter(sizes.columns.size == size, sizes.columns.quantity > 0))
-    is_available = t.fetchone()
-    if is_available:
-        stmt = sizes.update().where(sizes.columns.size == size).values(quantity=(sizes.columns.quantity - 1))
-        connection.execute(stmt)
+def _size_reservation(req_size):
+    size_quant = connection.execute(db.select(sizes.columns.quantity).filter(sizes.columns.size == req_size)).fetchone()
+    claimed_sizes = connection.execute(db.select([db.func.count(claimed.columns.size)]).filter(claimed.columns.size == req_size)).fetchone()
+
+    if size_quant['quantity'] - claimed_sizes['count_1']:
         return True
     return False
 
 
+def _sizes_left():
+    sizes_available = connection.execute(db.select([sizes])).fetchall()
+    size_dict = {}
+    for d in sizes_available:
+        size_dict[d['size']] = d['quantity']
+
+    sizes_claimed = connection.execute(db.select(claimed.columns.size).filter(claimed.columns.size != ''))
+    for item in sizes_claimed:
+        size_dict[item['size']] -= 1
+        if size_dict[item['size']] == 0:
+            size_dict.pop(item['size'])
+    return {'sizes': list(size_dict.keys())}
+
+
+
 def _we_counting_here(url_token):
     stmt = claimed.update().where(claimed.columns.url_token == url_token).values(callCount=(claimed.columns.callCount + 1))
-    test = connection.execute(stmt)
+    connection.execute(stmt)
 
 
 if __name__ == "__main__":
